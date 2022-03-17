1ca17aff3ae53026792a8781bd52147b2124a64e
sj <64534668+sj-h4@users.noreply.github.com>
2022-03-17 19:32:11 +0900

    Change appbar

--- lib/ui/widgets/rotation.dart
+++ lib/ui/widgets/rotation.dart
@@ -1,3 +1,5 @@
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
 import 'widgets.dart';
 import '../controllers/vector3_controller.dart';
 
@@ -7,11 +9,26 @@ class Rotation extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final rotation = ref.watch(vector3ControllerProvider).rotation;
-    return Text(
-      'rotation: $rotation',
-      textAlign: TextAlign.center,
-      overflow: TextOverflow.ellipsis,
-      style: const TextStyle(fontWeight: FontWeight.bold),
-    );
+    return Center(
+        child: SfRadialGauge(
+      axes: <RadialAxis>[
+        RadialAxis(minimum: 60, maximum: 150, pointers: <GaugePointer>[
+          RangePointer(
+            value: rotation!,
+            width: 20,
+            color: Colors.teal,
+          ),
+        ], annotations: <GaugeAnnotation>[
+          GaugeAnnotation(
+              angle: 90,
+              positionFactor: 0.5,
+              widget: Text(
+                '${(rotation).toStringAsFixed(1)} rpm',
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ))
+        ])
+      ],
+    ));
   }
 }
